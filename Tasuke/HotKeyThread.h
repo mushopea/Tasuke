949203753cd4604fec86e75665ae25c73267b6dc
Fazli Sapuan <fazli@sapuan.org>
2014-03-01 18:49:53 +0800

    Added thread for global hot key

---
+++ Tasuke/HotKeyThread.h
@@ -0,0 +1,28 @@
+#ifndef HOTKEYTHREAD_H
+#define HOTKEYTHREAD_H
+
+#include <QThread>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+class HotKeyThread : public QThread {
+	Q_OBJECT
+
+public:
+	HotKeyThread(QObject *parent);
+	~HotKeyThread();
+
+signals:
+	void hotKeyPress(int);
+
+public slots:
+	void run();
+	void stop();
+	
+private:
+	volatile bool stopped;
+	DWORD pid;
+};
+
+#endif
