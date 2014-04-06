e8829421d58d702abf6c3bc96420b08359a31ad6
Fazli Sapuan <fazli@sapuan.org>
2014-04-06 19:24:21 +0800

    Fixed more tooltip still visible bugs

---
+++ Tasuke/InputWindow.cpp
@@ -0,0 +1,181 @@
+#include "Tasuke.h"
+#include "InputWindow.h"
+#include "Interpreter.h"
+
+InputWindow::InputWindow(QWidget* parent) : QWidget(parent) {
+	LOG(INFO) << "InputWindow instance created";
+
+	ui.setupUi(this);
+	highlighter = new InputHighlighter(ui.lineEdit->document());
+	tooltipWidget = new TooltipWidget(this);
+	ui.lineEdit->installEventFilter(this);
+	initAnimation();
+
+	setAttribute(Qt::WA_TranslucentBackground);
+    setStyleSheet("background:transparent;");
+	setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint | Qt::Tool);
+
+	connect(ui.lineEdit, SIGNAL(textChanged()), this, SLOT(handleLineEditChanged()));
+}
+
+InputWindow::~InputWindow() {
+	LOG(INFO) << "InputWindow instance destroyed";
+}
+
+// Allows other classes to display a tooltip message relative to the status of the input.
+void InputWindow::showTooltipMessage(InputStatus status, QString message) {
+	if (!tooltipWidget->isVisible()) {
+		tooltipWidget->showAndAlign();
+	}
+	tooltipWidget->setText(status, message);
+}
+
+void InputWindow::hideTooltip() {
+	tooltipWidget->hide();
+}
+
+bool InputWindow::eventFilter(QObject* object, QEvent* event) {
+    if(event->type() == QEvent::KeyPress) {
+		// enter key
+		QKeyEvent* eventKey = static_cast<QKeyEvent*>(event);
+		if(eventKey->key() == Qt::Key_Return) {
+			handleReturnPressed();
+			return true;
+		}
+
+		if (Tasuke::instance().getTaskWindow().getScreen() == 0){
+			// Scroll keys for tasks
+			if (eventKey->modifiers() & Qt::CTRL) {
+				if (eventKey->key() == Qt::Key_Down) {
+					Tasuke::instance().getTaskWindow().pageDown();
+					return true;
+				}
+
+				if (eventKey->key() == Qt::Key_Up) {
+					Tasuke::instance().getTaskWindow().pageUp();
+					return true;
+				}
+			}
+
+			if (eventKey->key() == Qt::Key_Down) {
+				Tasuke::instance().getTaskWindow().scrollDown();
+				return true;
+			}
+
+			if (eventKey->key() == Qt::Key_Up) {
+				Tasuke::instance().getTaskWindow().scrollUp();
+				return true;
+			}
+
+			// undo keys
+			if (eventKey->matches(QKeySequence::Undo)) {
+				Tasuke::instance().runCommand(QString("undo"));
+			}
+
+			if (eventKey->matches(QKeySequence::Redo)) {
+				Tasuke::instance().runCommand(QString("redo"));
+			}
+
+			// paste shortcut
+			if (eventKey->matches(QKeySequence::Paste)) {
+				if(ui.lineEdit->toPlainText().trimmed().isEmpty()) {
+					ui.lineEdit->insertPlainText(QString("add "));
+				}
+			}
+		}
+
+		if (Tasuke::instance().getTaskWindow().getScreen() == 1) { // On tutorial window
+			if (eventKey->key() == Qt::Key_Tab){
+				Tasuke::instance().getTaskWindow().changeTutorialWidgetTabs();
+				return true;
+			}
+		}
+    }
+
+	if(event->type() == QEvent::FocusOut) {
+		closeAndClear();
+    }
+
+    return QObject::eventFilter(object, event);
+}
+
+
+void InputWindow::showAndCenter() {
+	LOG(INFO) << "Displaying input window";
+
+	QPoint pos = QApplication::desktop()->screen()->rect().center() - rect().center();
+	if(Tasuke::instance().getTaskWindow().isVisible()){ // If taskWindow is open
+		pos.setY(Tasuke::instance().getTaskWindow().y() + Tasuke::instance().getTaskWindow().height() + 3); //set commandbox below taskWindow
+		pos.setX(Tasuke::instance().getTaskWindow().x());
+	} else {
+		pos.setY(QApplication::desktop()->screen()->rect().height() / 4);
+	}
+
+	move(pos);
+
+	show(); 
+	raise(); 
+	activateWindow();
+	animation->start();
+}
+
+void InputWindow::showAndAdd() {
+	showAndCenter();
+	ui.lineEdit->insertPlainText(QString("add "));
+}
+
+void InputWindow::closeAndClear() {
+	hide();
+	ui.lineEdit->clear();
+}
+
+void InputWindow::handleReturnPressed() {
+	QString command = ui.lineEdit->toPlainText();
+
+	if (command.isEmpty()) {
+		return;
+	}
+
+	Tasuke::instance().runCommand(command);
+}
+
+void InputWindow::initAnimation() {
+	animation = new QPropertyAnimation(this, "opacity"); 
+	animation->setEasingCurve(QEasingCurve::OutCubic); 
+	animation->setDuration(700); 
+	animation->setStartValue(0.0); 
+	animation->setEndValue(0.95); 
+}
+
+// Will be updated when "themes" is implemented.
+void InputWindow::changeBorder(int themeNumber) {
+}
+
+// Will be updated when "themes" is implemented.
+void InputWindow::changeBG(int themeNumber) {
+}
+
+void InputWindow::handleLineEditChanged() {
+	QString currText = ui.lineEdit->toPlainText();
+
+	if (currText.isEmpty()) {
+		hideTooltip();
+	} else {
+		emit inputChanged(currText);
+	}
+}
+
+void InputWindow::setOpacity(qreal value) {
+	wOpacity = value;
+	setWindowOpacity(value);
+	update();
+}
+
+qreal InputWindow::getOpacity() const {
+	return wOpacity;
+}
+
+void InputWindow::hideEvent(QHideEvent* event) {
+	Q_UNUSED(event);
+	hideTooltip();
+}
---
+++ Tasuke/InputWindow.h
@@ -0,0 +1,57 @@
+#ifndef INPUTWINDOW_H
+#define INPUTWINDOW_H
+
+#include <QWidget>
+#include <glog/logging.h>
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QKeyEvent>
+#include <QFocusEvent>
+#include <QPropertyAnimation>
+#include "HotKeyThread.h"
+#include "ui_InputWindow.h"
+#include "InputHighlighter.h"
+#include "TooltipWidget.h"
+#include "Constants.h"
+
+class InputWindow : public QWidget {
+	Q_OBJECT
+	Q_PROPERTY(qreal opacity READ getOpacity WRITE setOpacity)
+
+public:
+	InputWindow(QWidget *parent = 0);
+	~InputWindow();
+	void showTooltipMessage(InputStatus status, QString message = "");
+	void hideTooltip();
+
+public slots:
+	void showAndCenter();	
+	void showAndAdd();
+	void closeAndClear();
+
+protected:
+	bool eventFilter(QObject *object, QEvent *event);
+	void hideEvent(QHideEvent *event);
+
+signals:
+	void inputChanged(QString text);
+
+private slots:
+	void handleReturnPressed();
+	void initAnimation();
+	void changeBorder(int themeNumber);
+	void changeBG(int themeNumber);
+	void handleLineEditChanged();
+
+private:
+	Ui::InputWindow ui;
+    InputHighlighter *highlighter;
+	TooltipWidget *tooltipWidget;
+	QPropertyAnimation *animation;
+	qreal wOpacity;
+
+	void setOpacity(qreal value);
+	qreal getOpacity() const;
+};
+
+#endif // INPUTWINDOW_H
