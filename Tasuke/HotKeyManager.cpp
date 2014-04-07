1db2e2cf5facf40d467663acf8b3bf64fc205fca
Fazli Sapuan <fazli@sapuan.org>
2014-04-07 18:22:11 +0800

    fix wednesday

---
+++ Tasuke/HotKeyManager.cpp
@@ -0,0 +1,28 @@
+#include <glog/logging.h>
+#include "Tasuke.h"
+#include "HotKeyManager.h"
+
+HotKeyManager::HotKeyManager(QObject *parent) : QObject(parent) {
+	hotKeyThread = new HotKeyThread(this);
+	connect(hotKeyThread, SIGNAL(hotKeyPress(KeyCombination)), this, SLOT(handleHotKeyPress(KeyCombination)), Qt::QueuedConnection);
+	hotKeyThread->start();
+}
+
+HotKeyManager::~HotKeyManager() {
+	if (hotKeyThread != nullptr) {
+		hotKeyThread->stop();
+		delete hotKeyThread;
+	}
+}
+
+void HotKeyManager::handleHotKeyPress(KeyCombination key) {
+	LOG(INFO) << "Hot key pressed with keycode " << (int)key;
+
+	if (key == KeyCombination::CTRL_SPACE) {
+		Tasuke::instance().toggleInputWindow();
+	} else if (key == KeyCombination::ALT_SPACE) {
+		Tasuke::instance().toggleTaskWindow();
+	} else if (key == KeyCombination::CTRL_ALT_SPACE) {
+		Tasuke::instance().toggleBothWindows();
+	}
+}
---
+++ Tasuke/Storage.h
@@ -0,0 +1,68 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <functional>
+#include <QString>
+#include <QList>
+#include "Task.h"
+
+// Interface class for Storage.
+class IStorage {
+protected:
+	//QList<Task> tasks;
+	QList<QSharedPointer<Task>> tasks;
+	QMutex mutex;
+
+public:
+	IStorage();
+	virtual ~IStorage();
+
+	Task addTask(Task& task);
+	Task editTask(int id, Task& task);
+	Task getTask(int id);
+	void removeTask(int id);
+	void popTask();
+	QList<Task> getTasks(bool hideDone = true) const;
+	int totalTasks();
+
+	QList<Task> search(std::function<bool(Task)> predicate) const;
+	QList<Task> searchByDescription(QString keyword, Qt::CaseSensitivity caseSensitivity = Qt::CaseInsensitive);
+	QList<Task> searchByTag(QString keyword, Qt::CaseSensitivity caseSensitivity = Qt::CaseInsensitive);
+	QList<Task> searchByEndDate(QDateTime byThisDate);
+	QList<Task> searchByBeginDate(QDateTime fromThisDate);
+	QList<Task> searchByDateTimeInterval(QDateTime fromThisDate, QDateTime byThisDate);
+
+	bool isAllDone();
+
+	void sortByEndDate();
+	void sortByBeginDate();
+	void sortByDescription();
+	void sortByOngoing();
+	void sortByIsDueToday();
+	void sortByDone();
+	void sortByOverdue();
+	void sortByHasEndDate();
+
+	void renumber();
+
+	void clearAllDone();
+	void clearAllTasks();
+
+	virtual void loadFile() = 0;
+	virtual void saveFile() = 0;
+};
+
+// This class abstracts away the data management in memory and on disk.
+// Usually only 1 instance of this class is required and it is managed by the
+// Tasuke singleton.
+class Storage : public IStorage {
+private:
+	QString path;
+public:
+	Storage();
+	Storage(QString path);
+	void loadFile();
+	void saveFile();
+};
+
+#endif
