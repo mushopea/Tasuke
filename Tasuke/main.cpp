66df9a833c4cbf5b624b14050aa3b26ddd6f1922
Fazli Sapuan <fazli@sapuan.org>
2014-04-09 10:04:50 +0800

    Make Tasuke single instance only

--- Tasuke/main.cpp
+++ Tasuke/main.cpp
@@ -1,12 +1,23 @@
 #include <glog/logging.h>
-#include <QtWidgets/QApplication>
+#include <QApplication>
+#include <QMessageBox>
+#include <QSharedMemory>
 #include "Tasuke.h"
 
 int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
+
+	QSharedMemory sharedMemory;
+	sharedMemory.setKey("Tasuke");
+	sharedMemory.attach();
+
+	if (!sharedMemory.create(1)) {
+		QMessageBox::information(nullptr, "Tasuke", "Tasuke is already running! Open Tasuke using ctrl+space");
+		return EXIT_FAILURE;
+	}
+
 	google::InitGoogleLogging(argv[0]);
 	LOG(INFO) << "Tasuke starting up!";
-
-	QApplication app(argc, argv);
 
 	QCoreApplication::setOrganizationName("Tasuke");
 	QCoreApplication::setApplicationName("Tasuke");
