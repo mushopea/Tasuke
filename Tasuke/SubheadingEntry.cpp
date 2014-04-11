3e61b4cb7679bdc7dbe209022974a8bcfc2863a8
mushopea <mushopea@localhost>
2014-04-12 02:43:33 +0800

    added consolas as a resource, but needs load.

---
+++ Tasuke/SubheadingEntry.cpp
@@ -0,0 +1,116 @@
+#include "Tasuke.h"
+#include "SubheadingEntry.h"
+#include <QSettings>
+
+//@author A0100189
+
+SubheadingEntry::SubheadingEntry(const QString& content, QWidget *parent): QWidget(parent), connectedToSettings(false) {
+	initUI();
+	initSettingsConnect();
+	reloadTheme();
+	changeText(content);
+}
+
+SubheadingEntry::~SubheadingEntry() {
+
+}
+
+void SubheadingEntry::changeText(const QString& text) {
+	ui.content->setText(text);
+}
+
+void SubheadingEntry::reloadTheme() {
+	LOG(INFO) << "Reloading theme in SubheadingEntry";
+	QSettings settings(QSettings::IniFormat, QSettings::UserScope, "Tasuke", "Tasuke");
+	Theme currTheme = (Theme)settings.value("Theme", (char)Theme::DEFAULT).toInt();
+
+	switch (currTheme) {
+	case Theme::DEFAULT:
+		applyDefaultTheme();
+		break;
+	case Theme::GREEN:
+		applyGreenTheme();
+		break;
+	case Theme::SPACE:
+		applySpaceTheme();
+		break;
+	case Theme::PINK:
+		applyPinkTheme();
+		break;
+	case Theme::PIKACHU:
+		applyPikaTheme();
+		break;
+	case Theme::BLUE:
+		applyBlueTheme();
+		break;
+	case Theme::DOGE:
+		applyDogeTheme();
+		break;
+	default:
+		break;
+	}
+}
+
+void SubheadingEntry::initUI() {
+	ui.setupUi(this);
+	setAttribute(Qt::WA_TranslucentBackground);
+	setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint | Qt::Tool);
+}
+
+void SubheadingEntry::initSettingsConnect() {
+	if (!connectedToSettings) {
+		connectedToSettings = true;
+		connect(&Tasuke::instance().getSettingsWindow(), SIGNAL(themeChanged()), this, SLOT(reloadTheme()));
+		LOG(INFO) << "Connected SubheadingEntry to SettingsWindow";
+	}
+}
+
+void SubheadingEntry::applyDefaultTheme() {
+	setStyleSheet("QLabel{\n"
+		"color: rgb(99, 99, 99);\n"
+		"}\n"
+		"background: transparent;");
+}
+
+void SubheadingEntry::applyGreenTheme() {
+	setStyleSheet("QLabel{\n"
+		"color: rgb(84,117,17);\n"
+		"}\n"
+		"background: transparent;");
+}
+
+void SubheadingEntry::applySpaceTheme() {
+	setStyleSheet("QLabel{\n"
+		"color: rgb(228,235,255);\n"
+		"}\n"
+		"background: transparent;");
+}
+
+void SubheadingEntry::applyPinkTheme() {
+	setStyleSheet("QLabel{\n"
+		"color: rgb(215,137,156);\n"
+		"}\n"
+		"background: transparent;");
+}
+
+void SubheadingEntry::applyPikaTheme() {
+	setStyleSheet("QLabel{\n"
+		"color: rgb(120,89,49);\n"
+		"}\n"
+		"background: transparent;");
+}
+
+void SubheadingEntry::applyBlueTheme() {
+	setStyleSheet("QLabel{\n"
+		"color: rgb(77,124,169);\n"
+		"}\n"
+		"background: transparent;");
+}
+
+void SubheadingEntry::applyDogeTheme() {
+	setStyleSheet("QLabel{\n"
+		"color: rgb(0,255,255);\n"
+		"}\n"
+		"background: transparent;"
+		"font: 75 16pt \"Comic Sans MS\";");
+}
