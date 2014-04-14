5f551b185918c24f95adcd504f51b29156ec34ce
Fazli Sapuan <fazli@sapuan.org>
2014-04-14 21:11:08 +0800

    Add more comments to main and Tasuke

---
+++ Tasuke/NotificationManager.h
@@ -0,0 +1,30 @@
+#ifndef NOTIFICATIONMANAGER_H
+#define NOTIFICATIONMANAGER_H
+
+//@author A0096863M
+
+#include <QTimer>
+#include "Task.h"
+
+// Class for the notification timer.
+class NotificationManager : public QObject {
+	Q_OBJECT
+private:
+	QTimer timer;
+	Task nextTask;
+
+	NotificationManager();
+	NotificationManager(const NotificationManager& old);
+	const NotificationManager& operator=(const NotificationManager& old);
+	~NotificationManager();
+
+public:
+	static NotificationManager &instance();
+	void init(void* storage);
+	void scheduleNotification(Task task);
+	
+public slots:
+	void handleTimeout();
+};
+
+#endif
